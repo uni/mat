6f586831f48e8e448fc5801dd9bfb9e90be53b68
UiWebKit <uniwebkit@gmail.com>
2022-01-04 20:16:24 +0200

    [2.0.0-4] uni-tab-bar add icons

--- dist/p-5ff5a78b.entry.js
+++
@@ -1,1 +0,0 @@
-export{U as uni_tab}from"./p-8088789d.js";import"./p-45e5d0c1.js";import"./p-c84d3d92.js";import"./p-82de3962.js";import"./p-12df985d.js";import"./p-e282851d.js";import"./p-e8183185.js";import"./p-bb796ab9.js";import"./p-66181af4.js";import"./p-7176408e.js";import"./p-a228c569.js";import"./p-d97f9e70.js";import"./p-9579904e.js";import"./p-cc523b0c.js";import"./p-1788acae.js";
--- dist/p-a3014054.entry.js
+++
@@ -1,1 +0,0 @@
-export{U as uni_tab_icon}from"./p-0b109e17.js";import"./p-45e5d0c1.js";import"./p-82de3962.js";import"./p-12df985d.js";import"./p-e282851d.js";import"./p-cc523b0c.js";import"./p-1788acae.js";
